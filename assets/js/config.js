388ae9fbae745bd8c727fac138fb41ced1882afc
EugenioAtencia <eu.atencia@gmail.com>
2026-05-22 21:00:18 +0200

    Usar URL absoluta del proxy interno

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 const SALERO_CONFIG = {
-  apiBase: '/api/wp/v2',
+  apiBase: 'https://salero.webagencia360.com/api/wp/v2',
   cmsApiBase: 'https://cms.webagencia360.com/wp-json/wp/v2',
   contactUrl: '/hablamos/',
   whatsappUrl: 'https://wa.me/34665688916?text=Hola%2C%20quiero%20hacer%20una%20cata%20digital%20con%20Salero%20Digital.',
