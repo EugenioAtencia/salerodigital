753a0d9946ee71b42ff06578fdd550212045dee9
EugenioAtencia <eu.atencia@gmail.com>
2026-06-01 21:12:44 +0200

    Actualizar configuración al dominio definitivo

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,8 @@
 const SALERO_CONFIG = {
-  apiBase: 'https://salero.webagencia360.com/api/wp/v2',
+  apiBase: '/api/wp/v2',
   cmsApiBase: 'https://cms.webagencia360.com/wp-json/wp/v2',
+  siteOrigin: 'https://agenciaconsalero.es',
+  legacyOrigin: 'https://salero.webagencia360.com',
   contactUrl: '/hablamos/',
   whatsappUrl: 'https://wa.me/34665688916?text=Hola%2C%20quiero%20hacer%20una%20cata%20digital%20con%20Salero%20Digital.',
   endpoints: {
