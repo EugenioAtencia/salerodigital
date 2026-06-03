a348545e1f6bef5476ba98ebe36be863990ea056
EugenioAtencia <eu.atencia@gmail.com>
2026-06-03 12:02:12 +0200

    Cache bust imagen de fondo acordeon servicios

--- assets/js/config.js
+++ assets/js/config.js
@@ -39,7 +39,7 @@ const SALERO_CONFIG = {
     const accordionCss = document.createElement('link');
     accordionCss.id = 'service-menu-accordion-css';
     accordionCss.rel = 'stylesheet';
-    accordionCss.href = '/assets/css/service-menu-accordion.css?v=1';
+    accordionCss.href = '/assets/css/service-menu-accordion.css?v=2';
     document.head.appendChild(accordionCss);
   }
 })();
