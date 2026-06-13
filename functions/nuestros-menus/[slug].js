4a32ecf57c591d7cc94774320240d79464931d5f
EugenioAtencia <eu.atencia@gmail.com>
2026-06-13 14:22:05 +0200

    Redirect inactive menu child routes to parent page

--- functions/nuestros-menus/[slug].js
+++ functions/nuestros-menus/[slug].js
@@ -1,5 +1,3 @@
 export async function onRequest(context) {
-  const slug = String(context.params.slug || '').replace(/^\/+|\/+$/g, '');
-  const target = slug ? `/nuestros-menus/detalle/?slug=${encodeURIComponent(slug)}` : '/nuestros-menus/';
-  return Response.redirect(new URL(target, context.request.url).toString(), 302);
+  return Response.redirect(new URL('/nuestros-menus/', context.request.url).toString(), 301);
 }
