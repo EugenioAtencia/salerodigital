9ee6624d78ae6e693bffd2cfa99a84a0a29b1e75
EugenioAtencia <eu.atencia@gmail.com>
2026-06-13 13:52:28 +0200

    Remove temporary dynamic handler test

--- functions/el-menu/[slug]/handler.js
+++
@@ -1,5 +0,0 @@
-import { handleDynamicDetailRequest } from '../../_shared/dynamic-detail-renderer.js';
-
-export async function onRequest(context) {
-  return handleDynamicDetailRequest(context, 'servicio');
-}
