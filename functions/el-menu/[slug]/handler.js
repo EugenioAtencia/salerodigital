5078f2bcebeb2f23c69291755bedd7a4cf02d440
EugenioAtencia <eu.atencia@gmail.com>
2026-06-13 13:48:41 +0200

    Test handler under bracket dir

---
+++ functions/el-menu/[slug]/handler.js
@@ -0,0 +1,5 @@
+import { handleDynamicDetailRequest } from '../../_shared/dynamic-detail-renderer.js';
+
+export async function onRequest(context) {
+  return handleDynamicDetailRequest(context, 'servicio');
+}
