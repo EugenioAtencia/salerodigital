65ec16f96b8cf4d762925176ea6ab1305c8f0500
EugenioAtencia <eu.atencia@gmail.com>
2026-06-13 14:09:54 +0200

    Route menu detail requests to the static fallback

--- functions/nuestros-menus/[slug].js
+++ functions/nuestros-menus/[slug].js
@@ -1,5 +1,3 @@
-import { handleDynamicDetailRequest } from '../_shared/dynamic-detail-renderer.js';
-
 export async function onRequest(context) {
-  return handleDynamicDetailRequest(context, 'menu');
+  return context.next();
 }
