32b7138d1d24f3ec2ea8a9dddb0d80a938ae8614
EugenioAtencia <eu.atencia@gmail.com>
2026-06-14 23:03:35 +0200

    Use improved case shell for non-static cases

--- functions/casos-de-exito/[slug].js
+++ functions/casos-de-exito/[slug].js
@@ -1,5 +1,5 @@
 import { handleCasoRequest } from '../_shared/caso-renderer-static.js';
-import { handleCaseDetailShell } from '../_shared/case-detail-shell.js';
+import { handleCaseDetailShell } from '../_shared/case-detail-shell-v2.js';
 
 export async function onRequest(context) {
   const slug = String(context.params.slug || '').split('/')[0];
