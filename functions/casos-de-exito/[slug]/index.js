8fbb50597a44ad937a1dfb03d64c6970b76b64b6
EugenioAtencia <eu.atencia@gmail.com>
2026-06-14 14:50:24 +0200

    Use robust case renderer on trailing slash route

--- functions/casos-de-exito/[slug]/index.js
+++ functions/casos-de-exito/[slug]/index.js
@@ -1,4 +1,4 @@
-import { handleCasoRequest } from '../../_shared/caso-renderer.js';
+import { handleCasoRequest } from '../../_shared/caso-renderer-v2.js';
 
 export async function onRequest(context) {
   return handleCasoRequest(context);
