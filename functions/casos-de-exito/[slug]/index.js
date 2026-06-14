d014a6b733eced4363988abfec1814bc6d1ef04f
EugenioAtencia <eu.atencia@gmail.com>
2026-06-14 14:48:08 +0200

    Prevent trailing slash case SSR from falling through to home

--- functions/casos-de-exito/[slug]/index.js
+++ functions/casos-de-exito/[slug]/index.js
@@ -1,11 +1,5 @@
 import { handleCasoRequest } from '../../_shared/caso-renderer.js';
 
 export async function onRequest(context) {
-  const response = await handleCasoRequest(context);
-
-  if (response && response.status >= 500 && typeof context.next === 'function') {
-    return context.next();
-  }
-
-  return response;
+  return handleCasoRequest(context);
 }
