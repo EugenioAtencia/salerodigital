df763ebd66cf52775c69c9d20e0bd7058b257b66
EugenioAtencia <eu.atencia@gmail.com>
2026-06-14 22:44:16 +0200

    Switch blog route to client renderer

--- functions/la-rebotica/[slug].js
+++ functions/la-rebotica/[slug].js
@@ -1,4 +1,4 @@
-import { handleBlogPostRequest } from '../_shared/blog-renderer.js';
+import { handleBlogPostRequest } from '../_shared/blog-renderer-client.js';
 
 export async function onRequest(context) {
   return handleBlogPostRequest(context);
