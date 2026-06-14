66284891884c24fb796bfbae0c854b44d4fd8f8a
EugenioAtencia <eu.atencia@gmail.com>
2026-06-14 18:49:14 +0200

    Use static JSON renderer on trailing slash case route

--- functions/casos-de-exito/[slug]/index.js
+++ functions/casos-de-exito/[slug]/index.js
@@ -1,4 +1,4 @@
-import { handleCasoRequest } from '../../_shared/caso-renderer-ajax.js';
+import { handleCasoRequest } from '../../_shared/caso-renderer-static.js';
 
 export async function onRequest(context) {
   return handleCasoRequest(context);
