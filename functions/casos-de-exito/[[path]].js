e93b0e38d083601533ed97364933de48e8058ba3
EugenioAtencia <eu.atencia@gmail.com>
2026-06-14 18:42:28 +0200

    Use Ajax case renderer on catch-all route

--- functions/casos-de-exito/[[path]].js
+++ functions/casos-de-exito/[[path]].js
@@ -1,4 +1,4 @@
-import { handleCasoRequest } from '../_shared/caso-renderer-v4.js';
+import { handleCasoRequest } from '../_shared/caso-renderer-ajax.js';
 
 export async function onRequest(context) {
   return handleCasoRequest(context);
