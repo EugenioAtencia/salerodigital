a8b576cc72d5876d118733a8945089dd15e36c81
EugenioAtencia <eu.atencia@gmail.com>
2026-06-14 22:42:38 +0200

    Bypass case SSR on trailing route

--- functions/casos-de-exito/[slug]/index.js
+++ functions/casos-de-exito/[slug]/index.js
@@ -1,5 +1,3 @@
-import { handleCasoRequest } from '../../_shared/caso-renderer-static.js';
-
 export async function onRequest(context) {
-  return handleCasoRequest(context);
+  return context.next();
 }
